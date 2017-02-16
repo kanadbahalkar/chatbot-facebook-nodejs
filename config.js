e24a40365932c8b27ce587454a71bf5022c9b041
Kanad Bahalkar <kanad@koalachat.com>
2017-02-16 15:53:30 -0500

    updated config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAF2dFyi0M4BAM6KBWUxxtNjpbOdmEePMVMqMhf3XGHMmLBsd8aDucCYoUR6ElfIjGDQBbr9rtUUK0u3nLJu1I6jBxf2uZBUTtnzxAoLZAxggw347BZBicn7sSAL2FGqudIOkP3l3l70eS3KdUcPGMmbIdjZBdoLgfOzCl6TIwZDZD',
+    FB_VERIFY_TOKEN: 'chatkoalachat',
+    API_AI_CLIENT_ACCESS_TOKEN: 'fe82ca64b4aa4507b3bb8ace27e738ca',
+    FB_APP_SECRET: 'fe726a364f340caecd0419d3e16624da',
+    SERVER_URL: "https://immense-dusk-29493.herokuapp.com/",
 };
